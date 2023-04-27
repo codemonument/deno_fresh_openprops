51a706f1e01d7e8f7219e32977406aa5938eced6
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 14:30:30 +0200

    export postcss as named variable

--- deps/postcss.ts
+++ deps/postcss.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/postcss@8.4.16/mod.js";
+export * as postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
 import postcssImportPlugin from "https://esm.sh/postcss-import@15.1.0";
 import postcssJitPropsPlugin from "https://esm.sh/postcss-jit-props@1.0.13";
 
