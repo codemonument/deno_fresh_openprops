1274188eef88606440572779f67a5f662a9dec39
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 14:24:59 +0200

    add postcss dependencies

---
+++ deps/postcss.ts
@@ -0,0 +1,6 @@
+export * from "https://deno.land/x/postcss@8.4.16/mod.js";
+import postcssImportPlugin from "https://esm.sh/postcss-import@15.1.0";
+import postcssJitPropsPlugin from "https://esm.sh/postcss-jit-props@1.0.13";
+
+export const postcssImport = postcssImportPlugin;
+export const postcssJitProps = postcssJitPropsPlugin;
