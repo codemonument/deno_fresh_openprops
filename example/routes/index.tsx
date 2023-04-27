7c1c114ea95fbd3461441d16911e45042612a102
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 17:31:00 +0200

    add example code to global.css and index.tsx

--- example/routes/index.tsx
+++ example/routes/index.tsx
@@ -5,8 +5,10 @@ export default function Home() {
     <>
       <Head>
         <title>OpenProps in Deno Fresh</title>
+        <link rel="stylesheet" href="/postcss/global.css" />
       </Head>
       <main> 
+        {/* <h1 class="gradient-text ">OpenProps in Deno Fresh</h1> */}
         <h1>OpenProps in Deno Fresh</h1>
       </main>
     </>
