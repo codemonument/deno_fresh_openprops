654c2e712b472137609f89d2100bd5c37b2369e1
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 15:23:59 +0200

    add disclaimer in mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,9 @@
 /**
  * Export all functionality of your module here,
  * which should be used by other people
+ * 
+ * IMPORTANT: This lib expects to be run in a deno repo with the fresh framework! 
+ * https://fresh.deno.dev/
  */
 
 export * from "./lib/css-cache/cssCache.ts"
