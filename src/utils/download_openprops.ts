f8408333248d61e9aeed01aabc2ed676cd20a09f
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 23:49:08 +0200

    extract function to download openprops css files

---
+++ src/utils/download_openprops.ts
@@ -0,0 +1,35 @@
+import { fs, path } from "../deps/std.ts";
+import { z, ZodSemver } from "../deps/zod.ts";
+
+const Options = z.object({
+  openPropsVersion: z.union([ZodSemver, z.literal("latest")]),
+  outPath: z.string().optional().default("css_deps/open-props"),
+});
+
+export async function downloadOpenprops(
+  { openPropsVersion, outPath }: z.infer<typeof Options>,
+) {
+  const baseUrl = `https://unpkg.com/open-props@${openPropsVersion}`;
+
+  await fs.ensureDir(outPath);
+  const openPropsMin = await (await fetch(`${baseUrl}/open-props.min.css`))
+    .text();
+  await Deno.writeTextFile(
+    path.join(outPath, "open-props.min.css"),
+    openPropsMin,
+  );
+
+  const normalize = await (await fetch(`${baseUrl}/normalize.min.css`))
+    .text();
+  await Deno.writeTextFile(
+    path.join(outPath, "normalize.min.css"),
+    normalize,
+  );
+
+  const buttons = await (await fetch(`${baseUrl}/buttons.min.css`))
+    .text();
+  await Deno.writeTextFile(path.join(outPath, "buttons.min.css"), buttons);
+
+  // write version file
+  await Deno.writeTextFile(path.join(outPath, "VERSION"), openPropsVersion);
+}
