a04edf08bbf5524d1a6c8ba28d55442c2eeb303e
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 18:18:36 +0200

    add export of fresh_openprops_plugin to mod.ts

--- mod.ts
+++ mod.ts
@@ -8,4 +8,9 @@
 
 export * from "./lib/css-cache/cssCache.ts";
 export * from "./lib/css-cache/postcssInstance.ts";
+
+// legacy, should be replaced by the fresh_openprops_plugin
 export * from "./lib/generatePostcssHandler.ts";
+
+// successor of generatePostcssHandler
+export * from "./lib/fresh_openprops_plugin.ts";
