b5d5ed3e69042e5425d1f966e9e2181b19ace428
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 23:28:46 +0200

    add path export to std.ts

--- src/deps/std.ts
+++ src/deps/std.ts
@@ -1,4 +1,5 @@
 export * as log from "https://deno.land/std@0.195.0/log/mod.ts";
 export { Logger } from "https://deno.land/std@0.195.0/log/mod.ts";
 export * as fs from "https://deno.land/std@0.195.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.195.0/path/mod.ts";
 export { encode as encodeBase64 } from "https://deno.land/std@0.195.0/encoding/base64.ts";
