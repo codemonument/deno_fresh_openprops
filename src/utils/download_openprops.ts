1bede4b260ece9cd5b756d33502bb151f723a2cf
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 23:59:44 +0200

    improve download script

--- src/utils/download_openprops.ts
+++ src/utils/download_openprops.ts
@@ -1,5 +1,6 @@
-import { fs, path } from "../deps/std.ts";
+import { fs, log, path } from "../deps/std.ts";
 import { z, ZodSemver } from "../deps/zod.ts";
+import { getLatestOpenProps } from "./getLatestOpenProps.ts";
 
 const Options = z.object({
   openPropsVersion: z.union([ZodSemver, z.literal("latest")]).optional()
@@ -10,8 +11,19 @@ const Options = z.object({
 export async function downloadOpenprops(
   options: { openPropsVersion?: string; outPath?: string },
 ) {
+  const logger = log.getLogger("FreshOpenProps");
   const { openPropsVersion, outPath } = Options.parse(options);
-  const baseUrl = `https://unpkg.com/open-props@${openPropsVersion}`;
+
+  const openPropsLatestVersion = await getLatestOpenProps();
+  const targetVersion = openPropsVersion === "latest"
+    ? openPropsLatestVersion
+    : openPropsVersion;
+
+  logger.info(
+    `downloadOpenprops with Version ${targetVersion} (latest: ${openPropsLatestVersion})`,
+  );
+
+  const baseUrl = `https://unpkg.com/open-props@${targetVersion}`;
 
   await fs.ensureDir(outPath);
   const openPropsMin = await (await fetch(`${baseUrl}/open-props.min.css`))
@@ -33,5 +45,5 @@ export async function downloadOpenprops(
   await Deno.writeTextFile(path.join(outPath, "buttons.min.css"), buttons);
 
   // write version file
-  await Deno.writeTextFile(path.join(outPath, "VERSION"), openPropsVersion);
+  await Deno.writeTextFile(path.join(outPath, "VERSION"), targetVersion);
 }
