0dde0f42ccfb362293580dbacb6a5e528822bc4d
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 18:37:34 +0200

    rename lib folder to src

--- mod.ts
+++ mod.ts
@@ -6,11 +6,11 @@
  * https://fresh.deno.dev/
  */
 
-export * from "./lib/css-cache/cssCache.ts";
-export * from "./lib/css-cache/postcssInstance.ts";
+export * from "./src/css-cache/cssCache.ts";
+export * from "./src/css-cache/postcssInstance.ts";
 
 // legacy, should be replaced by the fresh_openprops_plugin
-export * from "./lib/generatePostcssHandler.ts";
+export * from "./src/generatePostcssHandler.ts";
 
 // successor of generatePostcssHandler
-export * from "./lib/fresh_openprops_plugin.ts";
+export * from "./src/fresh_openprops_plugin.ts";
