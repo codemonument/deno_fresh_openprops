24370d1d360f9458114525ae9a29a862b2615b83
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 12:38:48 +0200

    upgrade openprops to 1.5.10

--- lib/css-cache/postcssInstance.ts
+++ lib/css-cache/postcssInstance.ts
@@ -5,7 +5,7 @@ import postcssImport from "https://esm.sh/postcss-import@15.1.0";
 import postcssJitProps from "https://esm.sh/postcss-jit-props@1.0.13";
 // See: https://www.npmjs.com/package/open-props
 // IMPORTANT: do not re-export this url from a deps.ts file, will break!
-import OpenProps from "https://esm.sh/open-props@1.5.8";
+import OpenProps from "https://esm.sh/open-props@1.5.10";
 import pDefer from "https://esm.sh/p-defer@4.0.0";
 
 const deferred = pDefer<ReturnType<typeof postcss>>();
