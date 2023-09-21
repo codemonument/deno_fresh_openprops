bdd6194a1d5e037848130239fbafdd80c1001d35
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-09-21 22:33:15 +0200

    extract handler for better readability

--- src/fresh_openprops_plugin.ts
+++ src/fresh_openprops_plugin.ts
@@ -66,71 +66,73 @@ export async function FreshOpenProps(rawOptions?: RawPluginOptions) {
     await prefillCssCache({ cssInputPath });
   }
 
-  const postcssRoute = {
-    path: "/postcss/[...path]",
-    handler: async (
-      req: Request,
-      ctx: HandlerContext,
-    ): Promise<Response> => {
-      const webservPath = ctx.params.path;
-      const fsPath = `${cssInputPath}/${webservPath}`;
-      const reqEtag = req.headers.get(`If-None-Match`);
+  const handler = async (
+    req: Request,
+    ctx: HandlerContext,
+  ): Promise<Response> => {
+    const webservPath = ctx.params.path;
+    const fsPath = `${cssInputPath}/${webservPath}`;
+    const reqEtag = req.headers.get(`If-None-Match`);
 
-      // Load input css file
-      const { fileHash, rawCssContent } = await loadCss(fsPath);
+    // Load input css file
+    const { fileHash, rawCssContent } = await loadCss(fsPath);
 
-      // validate etag sent by client agains the requested file
-      // slice away first two chars, bc. they indicate weak etag comparison with "W/"
-      if (reqEtag !== null && reqEtag.slice(2) === fileHash) {
-        return new Response(null, {
-          status: 304,
-          statusText: "Not Modified",
-          headers: new Headers([
-            ["ETag", reqEtag],
-          ]),
-        });
-      }
-
-      // Check cache
-      if (!await cssCache.has(fileHash)) {
-        // On cache miss: process css file and cache it
-        await processAndCacheCss({ fsPath, rawCssContent, fileHash });
-      }
-
-      // Get storedCSS from cache
-      const storedCSSResult = z.string().safeParse(
-        await cssCache.get(fileHash),
-      );
-
-      if (storedCSSResult.success === false) {
-        return new Response(null, {
-          status: 500,
-          statusText:
-            "Css not available in cache after processing, should not happen!",
-        });
-      }
-
-      logger.debug(`PostCSS found in cache: ${fsPath}`, { fileHash });
-
-      const cachingHeader: [string, string][] = isProd
-        ? [
-          // Cache the css files for min 1h (3600sek) and max-age=604800, then use the old file while revalidating
-          [
-            "Cache-Control",
-            "public, max-age=604800, stale-while-revalidate=3600",
-          ],
-          ["ETag", fileHash],
-        ]
-        : [];
-
-      // deliver stored css
-      return new Response(storedCSSResult.data, {
+    // validate etag sent by client agains the requested file
+    // slice away first two chars, bc. they indicate weak etag comparison with "W/"
+    if (reqEtag !== null && reqEtag.slice(2) === fileHash) {
+      return new Response(null, {
+        status: 304,
+        statusText: "Not Modified",
         headers: new Headers([
-          ["Content-Type", "text/css"],
-          ...cachingHeader,
+          ["ETag", reqEtag],
         ]),
       });
-    },
+    }
+
+    // Check cache
+    if (!await cssCache.has(fileHash)) {
+      // On cache miss: process css file and cache it
+      await processAndCacheCss({ fsPath, rawCssContent, fileHash });
+    }
+
+    // Get storedCSS from cache
+    const storedCSSResult = z.string().safeParse(
+      await cssCache.get(fileHash),
+    );
+
+    if (storedCSSResult.success === false) {
+      return new Response(null, {
+        status: 500,
+        statusText:
+          "Css not available in cache after processing, should not happen!",
+      });
+    }
+
+    logger.debug(`PostCSS found in cache: ${fsPath}`, { fileHash });
+
+    const cachingHeader: [string, string][] = isProd
+      ? [
+        // Cache the css files for min 1h (3600sek) and max-age=604800, then use the old file while revalidating
+        [
+          "Cache-Control",
+          "public, max-age=604800, stale-while-revalidate=3600",
+        ],
+        ["ETag", fileHash],
+      ]
+      : [];
+
+    // deliver stored css
+    return new Response(storedCSSResult.data, {
+      headers: new Headers([
+        ["Content-Type", "text/css"],
+        ...cachingHeader,
+      ]),
+    });
+  };
+
+  const postcssRoute = {
+    path: "/postcss/[...path]",
+    handler,
   } satisfies PluginRoute;
 
   return {
